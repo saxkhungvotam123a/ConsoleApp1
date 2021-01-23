861d69e50a1dd2672396ac4e930a4c5f47b1981d
Vu Duc <saxkhungvotam123a@gmail.com>
2021-01-23 08:10:13 +0700

    Feature2-2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Feature2-1!");
+            Console.WriteLine("Feature2-2!");
         }
     }
 }
