50c049e92e135d63d37b218899c09477d81ad77b
Vu Duc <saxkhungvotam123a@gmail.com>
2021-01-23 09:11:08 +0700

    Develop 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Feature3-2!");
+            Console.WriteLine("Develop 2!");
         }
     }
 }
